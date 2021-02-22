818843347e594c2b78555ee2e1f82e95e42f88d9
Plofland <ploflan@gmail.com>
2021-02-22 17:02:06 -0500

    Built auth endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../users/users-model');
+const bcrypt = require('bcryptjs');
 const {
   checkPayload,
   checkUserInDb,
@@ -10,11 +11,51 @@ router.post(
   '/register',
   checkPayload,
   checkUserInDb,
-  (req, res) => {}
+  async (req, res) => {
+    try {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = await User.add({
+        username: req.body.username,
+        password: hash
+      });
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
 );
 
-router.post('/login', checkUserExists, (req, res) => {});
+router.post('/login', checkUserExists, (req, res) => {
+  try {
+    const verified = bcrypt.compareSync(
+      req.body.password,
+      req.userData.password
+    );
+    if (verified) {
+      req.session.user = req.userData;
+      res.json(`Welcome back ${req.userData.username}`);
+    } else {
+      res
+        .status(401)
+        .json('Username or password are incorrect');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-router.get('/logout', (req, res) => {});
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.json('Cannot log out');
+      } else {
+        res.json('Successfully logged out');
+      }
+    });
+  } else {
+    res.json('No session');
+  }
+});
 
 module.exports = router;
