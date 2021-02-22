7a82a0e6e6f9106955e948ad46fc25ea0385f847
Plofland <ploflan@gmail.com>
2021-02-22 16:32:31 -0500

    Built out migrations

---
+++ database/migrations/20210222162439_00-init.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('roles', (table) => {
+      table.increments();
+      table.string('name', 128).notNullable().unique();
+    })
+    .createTable('users', (table) => {
+      table.increments();
+      table
+        .string('username', 128)
+        .notNullable()
+        .unique()
+        .index();
+      table.string('username', 256).notNullable();
+      table
+        .integer('role')
+        .unsigned()
+        .references('roles.id')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('roles');
+};
